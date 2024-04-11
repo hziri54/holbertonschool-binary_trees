181aabe83822756204ad8df8a6ec79aafc605e78
hziri54 <zirihouamdi64@gmail.com>
2024-04-11 10:30:53 +0000

    suppression d'espace dans le code pour betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - fonction qui vérifie si un noeud est une racine
+ * binary_tree_is_root - Vérifie si un nœud est une racine.
  *
- * @node : est un pointeur sur le nœud à vérifier.
+ * @node: Pointeur vers le nœud à vérifier.
  *
- * Retour : 1 si le noeud est une racine, sinon 0.
-*/
-
+ * Return: Retourne 1 si le nœud est une racine, sinon 0.
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
